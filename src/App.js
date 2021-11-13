701a86b01e6d0dc409f558e69bf457c05a04f322
Rodolpho Becker <becker.rodolpho@gmail.com>
2021-11-13 11:55:40 -0300

    [update] Pages Component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./components";
-import './App.css';
+import {
+	Navbar,
+	Exchanges,
+	HomePage,
+	Cryptocurrencies,
+	News,
+	CryptoDetails,
+} from "./components";
+
+import "./App.css";
 
 const App = () => {
 	return (
@@ -12,7 +19,19 @@ const App = () => {
 			<div className="navbar">
 				<Navbar />
 			</div>
-			<div className="main"></div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						<Routes>
+							<Route exact path="/" element={<HomePage />} />
+							<Route exact path="/exchanges" element={<Exchanges />} />
+							<Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+							<Route exact path="/crypt/:coinId" element={<CryptoDetails />} />
+							<Route exact path="/news" element={<News />} />
+						</Routes>
+					</div>
+				</Layout>
+			</div>
 			<div className="footer"></div>
 		</div>
 	);
