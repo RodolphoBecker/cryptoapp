a39f12db82a4a0fb36c10277fe165fde99fd400c
Rodolpho Becker <becker.rodolpho@gmail.com>
2021-11-13 12:00:52 -0300

    [update] Footer created

--- src/App.js
+++ src/App.js
@@ -25,14 +25,31 @@ const App = () => {
 						<Routes>
 							<Route exact path="/" element={<HomePage />} />
 							<Route exact path="/exchanges" element={<Exchanges />} />
-							<Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+							<Route
+								exact
+								path="/cryptocurrencies"
+								element={<Cryptocurrencies />}
+							/>
 							<Route exact path="/crypt/:coinId" element={<CryptoDetails />} />
 							<Route exact path="/news" element={<News />} />
 						</Routes>
 					</div>
 				</Layout>
+				<div className="footer">
+					<Typography.Title
+						level={5}
+						style={{ color: "white", textAlign: "center" }}
+					>
+						Cryptoverse <br />
+						All Rights Reserved
+					</Typography.Title>
+					<Space>
+						<Link to="/">Home</Link>
+						<Link to="/exchanges">Exchanges</Link>
+						<Link to="/news">News</Link>
+					</Space>
+				</div>
 			</div>
-			<div className="footer"></div>
 		</div>
 	);
 };
